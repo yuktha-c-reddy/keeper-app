db7611011dd481b3e14d22e6c256a2e359a664fc
Yuktha C <70469290+yuktha-c-reddy@users.noreply.github.com>
2023-04-15 13:44:18 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./Header";
+import Header from "Header.jsx";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
